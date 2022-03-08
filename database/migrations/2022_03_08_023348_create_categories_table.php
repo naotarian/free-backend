c33da9df60064d68d27a3f087b8f7f053f90862f
naotarian <naotarian19@gmail.com>
2022-03-08 02:59:45 +0000

    カテゴリーテーブル形

---
+++ database/migrations/2022_03_08_023348_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128)->comment('カテゴリー名');
+            $table->integer('display_order')->nullable()->default(null)->comment('表示順');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
